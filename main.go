e69731799af1685f56f401fe9ec5d93417ee5c51
Jerry Young <jerryyoung2911@gmail.com>
2021-12-14 10:49:59 +0700

    Remove Old Files

--- main.go
+++ main.go
@@ -4,8 +4,6 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello World")
-	fmt.Println("Hello World 2")
-	fmt.Println("Halo From Development")
 	fmt.Println("New Update from fitur 1")
 	fmt.Println("Update from fitur 1 part 1")
 	fmt.Println("Fitur 1 DONE")
