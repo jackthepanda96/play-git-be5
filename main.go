80113e05ad8e9aec4addea4395f923c15993d7b6
Jerry Young <jerryyoung2911@gmail.com>
2021-12-14 10:48:23 +0700

    Add New Rules

--- main.go
+++ main.go
@@ -4,8 +4,8 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello World")
-	fmt.Println("Hello World 2")
-	fmt.Println("Halo From Development")
+	fmt.Println("Hello World 3")           // PENTING
+	fmt.Println("Halo From Development 5") // WAJIB
 	fmt.Println("New Update from fitur 1")
 	fmt.Println("Update from fitur 1 part 1")
 	fmt.Println("Fitur 1 DONE")
